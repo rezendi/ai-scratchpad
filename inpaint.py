66fb0137f459c8b7f002587e5c89eaf4ec40ce22
Jon Evans <jon@rezendi.com>
2022-12-07 20:36:16 -0800

    Update inpaint.py

--- inpaint.py
+++ inpaint.py
@@ -1,49 +1,68 @@
-import os, time, uuid
+import os, sys, time, uuid
 import cv2, numpy, torch
 from PIL import Image, ImageOps
 from matplotlib import pyplot as plt
 from diffusers import StableDiffusionInpaintPipeline, EulerDiscreteScheduler
 from transformers import CLIPSegProcessor, CLIPSegForImageSegmentation
 
-model_id = "stabilityai/stable-diffusion-2-inpainting"
-WIDTH = 768
-HEIGHT = 768
-image_file = "./images/a_cowboy_riding_a_triceratops-1670270324.png"
+if len(sys.argv) < 4:
+  print("Please enter an original image, text to identify what to retain, and a prompt with which to replace the rest")
+  print("You may additionally optionally enter an output filename and the number of inpainting iterations")
+  quit()
 
-# generate the mask for the image
-mask_prompts = ['the cowboy']
-cliptime1 = time.time()
+# parse the args
+image_file = sys.argv[1]
+mask_prompts = sys.argv[2].split(",")
+prompt = sys.argv[3]
+filename = sys.argv[2] if len(sys.argv) >2 else prompt.replace(" ","_")
+filename = filename + "-" + str(int(time.time())) + ".png"
+output_file = sys.argv[4] if len(sys.argv) >4 else "%s_inpainted" % image_file
+steps = int(sys.argv[5]) if len(sys.argv) > 5 else 10
+
+# define our models
+model_id = "stabilityai/stable-diffusion-2-inpainting"
 processor = CLIPSegProcessor.from_pretrained("CIDAS/clipseg-rd64-refined")
 model = CLIPSegForImageSegmentation.from_pretrained("CIDAS/clipseg-rd64-refined")
-image = Image.open(image_file)
-mask_prompts = ["a cowboy"]
-inputs = processor(text=mask_prompts, images=[image] * len(mask_prompts), padding=True, return_tensors="pt")
+
+# load initial image, size it down to something reasonable if needed
+# TODO: ensure both dimensions are multiples of 8, which something downstream requires
+original = Image.open(image_file)
+while original.width > 1024:
+  original = original.resize((original.width // 2, original.height // 2))
+print("Immage dimensions %d x %d" % (original.width, original.height))
+
+# Get rid of alpha channel, repurposed from https://stackoverflow.com/a/35859141
+if original.mode in ('RGBA', 'LA') or (original.mode == 'P' and 'transparency' in original.info):
+  converted = original.convert('RGBA')
+  background = Image.new('RGBA', converted.size, (255,255,255))
+  alpha_composite = Image.alpha_composite(background, converted)
+  alpha_composite_3 = alpha_composite.convert('RGB')
+  original = alpha_composite_3
+
+# generate mask tensor from the CLIPseg model
+inputs = processor(text=mask_prompts, images=[original] * len(mask_prompts), padding=True, return_tensors="pt")
 outputs = model(**inputs)
 sigmoided = torch.sigmoid(outputs.logits)
 detached = sigmoided.detach().numpy()
 
 # we have a tensor, make a mask image
-filename = f"{uuid.uuid4()}.png"
-plt.imsave(filename, detached)
-img2 = cv2.imread(filename)
+tempfilename = f"{uuid.uuid4()}.png"
+plt.imsave(tempfilename, detached)
+img2 = cv2.imread(tempfilename)
 gray_image = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 (thresh, bw_image) = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)
 cv2.cvtColor(bw_image, cv2.COLOR_BGR2RGB)
 mask = Image.fromarray(numpy.uint8(bw_image)).convert('RGB')
-mask = mask.resize((WIDTH, HEIGHT))
+mask = mask.resize((original.width, original.height))
 mask = ImageOps.invert(mask)
-os.remove(filename)
-mask.save("mask_"+filename)
-cliptime2 = time.time()
-print("CLIPseg time: %d" % int(cliptime2 - cliptime1))
+os.remove(tempfilename)
+# uncomment to visually inspect the mask image
+# mask.save("mask_"+tempfilename) 
 
 # now do the inpainting
-original = Image.open(image_file)
-
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.has_mps else "cpu")
-pipe = StableDiffusionInpaintPipeline.from_pretrained(model_id, image=original, safety_checker=None).to(device)
+pipe = StableDiffusionInpaintPipeline.from_pretrained(model_id, safety_checker=None).to(device)
 pipe.enable_attention_slicing()
-prompt = "a dense jungle"
-image = pipe(prompt, image=original, mask_image=mask, height=HEIGHT, width=WIDTH, num_inference_steps=25).images[0]
 
-image.save("inpainted_%d.png" % int(time.time()))
+image = pipe(prompt, image=original, mask_image=mask, height=original.height, width=original.width, num_inference_steps=steps).images[0]
+image.save("%s_%d.png" % (output_file, int(time.time())))
